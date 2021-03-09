61269b85f855e6a402d3cd3259b1587ad47c9c67
nikurou <dylan.chung1997@gmail.com>
2021-03-08 22:20:55 -0800

    dynamic header title

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Platform } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMealsScreen = (props) => {
+  const categoryId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(
+    (category) => category.id === categoryId
+  );
+
   return (
     <View style={styles.screen}>
       <Text>This is CategoryMealsScreen</Text>
+      <Text>{selectedCategory.title}</Text>
       <View style={styles.buttonStyle}>
         <Button
           title="Go to Meals Details"
@@ -23,6 +31,16 @@ const CategoryMealsScreen = (props) => {
       </View>
     </View>
   );
+};
+
+//Header Title and Styling that is dynamic based on Changing Data
+// RN automatically passes in navigationData you use navigationOptions as a function.
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const categoryId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === categoryId);
+  return {
+    headerTitle: selectedCategory.title,
+  };
 };
 
 const styles = StyleSheet.create({
