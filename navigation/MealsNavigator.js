c7f0e05fe0087c36f0e167afb206cda2c74a3a8b
nikurou <dylan.chung1997@gmail.com>
2021-03-08 22:20:18 -0800

    header styling

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import CatogoriesScreen from "../screens/CategoriesScreen";
@@ -7,17 +6,40 @@ import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
 import FiltersScreen from "../screens/FiltersScreen";
 import MealDetailsScreen from "../screens/MealDetailsScreen";
+import Colors from "../constants/Colors";
+import { Platform } from "react-native";
+import CategoriesScreen from "../screens/CategoriesScreen";
 
 const MealsNavigator = createStackNavigator(
   {
-    Categories: CatogoriesScreen,
-    CategoryMeals: CategoryMealsScreen,
+    Categories: {
+      screen: CategoriesScreen,
+      navigationOptions: { headerTitle: "Meal Categories" },
+    },
+    CategoryMeals: {
+      headerTitle: "Meal Categories!!",
+      screen: CategoryMealsScreen,
+    },
     MealDetail: MealDetailsScreen,
     Filter: FiltersScreen,
     Favorites: FavoritesScreen,
   },
   {
     initialRouteName: "Categories",
+    //Default header styling
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor:
+          Platform.OS === "android" || Platform.OS === "web"
+            ? Colors.primaryColor
+            : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" || Platform.OS === "web"
+          ? "#ffff"
+          : Colors.primaryColor,
+    },
+    mode: "modal", //depending on modal or card, you get different animations when switching screens on IOS
   }
 );
 
