f659dfccc20427074122be81646c2c61b67bb2f5
nikurou <dylan.chung1997@gmail.com>
2021-03-11 18:29:40 -0800

    Card like UI to display meal information

---
+++ components/MealItem.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+
+const MealItem = (props) => {
+  return (
+    <View style={styles.mealItem}>
+      <TouchableOpacity onPress={props.onSelectMeal}>
+        <View>
+          <View style={[styles.mealRow, styles.mealHeader]}>
+            <ImageBackground
+              source={{
+                uri: props.image,
+              }}
+              style={styles.bgImage}
+            >
+              <View style={styles.titleContainer}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {props.itemData.item.title}
+                </Text>
+              </View>
+            </ImageBackground>
+          </View>
+          <View style={[styles.mealRow, styles.mealDetail]}>
+            <Text>{props.itemData.item.duration}m</Text>
+            <Text>{props.itemData.item.complexity.toUpperCase()}</Text>
+            <Text>{props.itemData.item.affordability.toUpperCase()}</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mealItem: {
+    height: 200,
+    width: "100%",
+    backgroundColor: "#ccc",
+    marginBottom: 10,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  mealRow: {
+    flexDirection: "row",
+  },
+  mealHeader: {
+    height: "85%",
+  },
+  mealDetail: {
+    paddingHorizontal: 10,
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "15%",
+  },
+  bgImage: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "green",
+    justifyContent: "flex-end",
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.6)",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 20,
+    color: "white",
+    paddingVertical: 5,
+    paddingHorizontal: 12,
+    textAlign: "center",
+  },
+});
+
+export default MealItem;
