04dcdd0f795c17a16e0da3a7290b1f869e1777d7
nikurou <dylan.chung1997@gmail.com>
2021-03-11 18:27:57 -0800

    Meal information passed, dynamic header naming

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../components/HeaderButton";
 
 const MealDetailsScreen = (props) => {
+  const mealId = props.navigation.getParam("mealId");
+
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>This is MealDetailsScreen</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title="Go back to Categories"
         onPress={() => {
@@ -13,6 +20,27 @@ const MealDetailsScreen = (props) => {
       />
     </View>
   );
+};
+
+//Header Title and Styling that is dynamic based on Changing Data
+// RN automatically passes in navigationData you use navigationOptions as a function.
+MealDetailsScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star-outline"
+          onPress={() => {
+            console.log("favorited");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
