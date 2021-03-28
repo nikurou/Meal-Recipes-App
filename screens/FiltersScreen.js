d3f0c6d818c1f9f115c3b6efe272983b8c723cfd
nikurou <dylan.chung1997@gmail.com>
2021-03-28 16:29:49 -0700

    added drawer to screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 const FiltersScreen = (props) => {
   return (
@@ -9,8 +11,21 @@ const FiltersScreen = (props) => {
   );
 };
 
-FiltersScreen.navigationOptions = {
-  headerTitle: "Filter Meals",
+FiltersScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Filter Meals",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
