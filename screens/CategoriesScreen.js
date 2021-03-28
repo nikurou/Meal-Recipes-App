065ceb4d0968fceac92a25b5817fce08aafb2e6f
nikurou <dylan.chung1997@gmail.com>
2021-03-27 20:54:18 -0700

    Hamburger Navigation Bar Added

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,6 +3,9 @@ import { StyleSheet, FlatList } from "react-native";
 import CategoryGridTile from "../components/CategoryGridTile";
 import { CATEGORIES } from "../data/dummy-data";
 
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
     return (
@@ -27,6 +30,23 @@ const CategoriesScreen = (props) => {
       keyExtractor={(item, index) => item.id}
     />
   );
+};
+
+CategoriesScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Meal Categories",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
