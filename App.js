3c845d0527f23e2d79df45ee9bf950dbf2483aa1
nikurou <dylan.chung1997@gmail.com>
2021-04-13 11:27:07 -0700

    Reducer loaded into App.js

--- App.js
+++ App.js
@@ -4,10 +4,21 @@ import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading"; //prolongs apploading until all our fonts/assets are loaded
 import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
 
 enableScreens();
+
+// If we had multiple reducers to manage mulitple things, we could use this.
+// Though we only have one in this case.
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+//Pass the combined reducers to the store
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -29,7 +40,12 @@ export default function App() {
     );
   }
 
-  return <MealsNavigator />;
+  //Behind the scenes this now gives us access to the redux state in any screen.
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
