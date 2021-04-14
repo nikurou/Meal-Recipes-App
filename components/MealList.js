2986947826092136651aaa758b5d6e7be9cb1638
nikurou <dylan.chung1997@gmail.com>
2021-04-13 18:25:02 -0700

    added isFav as a param of navigation prop

--- components/MealList.js
+++ components/MealList.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
+import { useSelector } from "react-redux";
 import MealItem from "./MealItem";
 
 const MealList = (props) => {
+  const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
+
   const renderMealItem = (itemData) => {
+    const isFavorite = favoriteMeals.some(
+      (meal) => meal.id === itemData.item.id
+    );
     return (
       <MealItem
         title={itemData.item.title}
@@ -15,6 +21,7 @@ const MealList = (props) => {
             params: {
               mealId: itemData.item.id,
               mealTitle: itemData.item.title,
+              isFav: isFavorite,
             },
           });
         }}
