b71e4ca872838ea09b93992eff6777bbe48e5844
nikurou <dylan.chung1997@gmail.com>
2021-04-13 18:24:46 -0700

    handling togglefavorite action for the reducer

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -3,6 +3,7 @@
 //This is called in APP.JS
 
 import { MEALS } from "../../data/dummy-data";
+import { TOGGLE_FAVORITE } from "../actions/meals";
 
 //Start with all the meals
 const initialState = {
@@ -12,6 +13,25 @@ const initialState = {
 };
 
 const mealsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case TOGGLE_FAVORITE:
+      const existingIndex = state.favoriteMeals.findIndex(
+        (meal) => meal.id === action.mealId
+      );
+      if (existingIndex >= 0) {
+        //it is already favorited, so unfavorite it.
+        const updatedFavMeals = [...state.favoriteMeals];
+        updatedFavMeals.splice(existingIndex, 1);
+        return { ...state, favoriteMeals: updatedFavMeals };
+      } else {
+        //It isn't favorited, so favorite it.
+        const meal = state.meals.find((meal) => meal.id === action.mealId);
+        return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) };
+      }
+      break;
+    default:
+      return state;
+  }
   return state;
 };
 
