bb0d8b146d091a0b41aab6e66f67ac5685e317f7
nikurou <dylan.chung1997@gmail.com>
2021-04-17 00:59:01 -0700

    setFilters action

--- store/actions/meals.js
+++ store/actions/meals.js
@@ -1,5 +1,11 @@
 export const TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
+export const SET_FILTERS = "SET_FILTERS";
 
 export const toggleFavorite = (id) => {
   return { type: TOGGLE_FAVORITE, mealId: id };
+};
+
+//Param: Expect to be an Object with {isGlucoseFree: true, isVegan: false...} etc.
+export const setFilters = (filterSetting) => {
+  return { type: SET_FILTERS, filters: filterSetting };
 };
