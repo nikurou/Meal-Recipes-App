17246abd7291a4001972585ba17cc2149d155e91
nikurou <dylan.chung1997@gmail.com>
2021-04-17 01:00:08 -0700

    Added default text if no results

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -4,10 +4,20 @@ import MealList from "../components/MealList";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
 import { useSelector } from "react-redux";
+import { StyleSheet, Text, View } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 const FavoritesScreen = (props) => {
   const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
-  return <MealList listData={favoriteMeals} navigation={props.navigation} />;
+  if (favoriteMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Favorite Meals Yet!</DefaultText>
+      </View>
+    );
+  } else {
+    return <MealList listData={favoriteMeals} navigation={props.navigation} />;
+  }
 };
 
 FavoritesScreen.navigationOptions = (navData) => {
@@ -26,5 +36,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     ),
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default FavoritesScreen;
