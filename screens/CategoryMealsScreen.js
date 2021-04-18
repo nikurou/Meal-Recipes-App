f8a19e24817e3fea72142613471d84d3f90935b4
nikurou <dylan.chung1997@gmail.com>
2021-04-17 22:29:53 -0700

    Added some default text if no meals found

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,21 +3,28 @@ import { useSelector } from "react-redux";
 import { CATEGORIES } from "../data/dummy-data";
 
 import MealList from "../components/MealList";
+import { StyleSheet, View } from "react-native";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealsScreen = (props) => {
   const categoryId = props.navigation.getParam("categoryId");
 
   //Taken from app.js's store
   // meals is the key given to mealsReducer in app.js
-  // Gives us access to that state slice, and we thus grab the filteredMeals from meals.js
+  // useSelector gives us access to that state slice, and we thus grab the filteredMeals from meals.js
   const availableMeals = useSelector((state) => state.meals.filteredMeals);
 
   const displayedMeals = availableMeals.filter(
     (meal) => meal.categoryIds.indexOf(categoryId) >= 0
   );
 
-  // Return list of mealItems
-  // Navigation property must be passed down.
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No Meals Found!</DefaultText>
+      </View>
+    );
+  }
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
@@ -30,5 +37,13 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default CategoryMealsScreen;
